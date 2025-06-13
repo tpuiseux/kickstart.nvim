af2a678e1251566c020ec2afe68a596de95fbe6c
tpuiseux <tpuiseux@spin-up.fr>
2025-06-13 09:22:00 +0200

    Add markdown preview neovim plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return { -- install without yarn or npm
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  ft = { 'markdown' },
+  build = function()
+    vim.opt.rtp:prepend(vim.fn.stdpath 'data' .. '/lazy/markdown-preview.nvim')
+    vim.fn['mkdp#util#install']()
+  end,
+}
